fd4eca04e7f45489bcd67ba59ff5d9b17d794ec3
Naftali MacBook 14 <naftaliko@gmail.com>
2024-05-27 13:52:12 +0900

    * Add new spinner. * Made so, for spinner to select current year, after open spinner.

--- app/src/main/java/com/nk/spinnercustomizetemp/MainActivity.java
+++ app/src/main/java/com/nk/spinnercustomizetemp/MainActivity.java
@@ -4,22 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.nk.spinnercustomizetemp.inventory.DataInventory;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Text_Code";
 
     private CustomSpinner fruitsSpinner, themesSpinner;
+    private Spinner yearSpinner;
     private FruitAdapter fruitAdapter;
     private ThemeSpinnerAdapter themeSpinnerAdapter;
     private ThemeSpinnerRecycleViewAdapter themeSpinnerRecycleViewAdapter;
     private ThemeSpinnerAdapterAdapter themeSpinnerAdapterAdapter;
+
+    private ArrayList<CharSequence> dates = new ArrayList<>();
+    private ArrayAdapter<CharSequence> yearsArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setReferences();
+        createYearsListAdapter();
 
         fruitAdapter = new FruitAdapter(this, DataInventory.getFruitList());
         fruitsSpinner.setAdapter(fruitAdapter);
@@ -71,10 +81,56 @@ public class MainActivity extends AppCompatActivity {
 
 //        themeSpinnerAdapterAdapter = new ThemeSpinnerAdapterAdapter(this);
 //        themesSpinner.setAdapter(themeSpinnerAdapterAdapter);
+
+        yearsArrayAdapter = new ArrayAdapter<CharSequence>(getBaseContext(), R.layout.spinner_item_text_block, dates);
+        yearSpinner.setAdapter(yearsArrayAdapter);
+        yearSpinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (yearSpinner.getSelectedItemPosition() == 0) {
+                    Log.d(TAG, "onTouch: Year not selected");
+
+                    Calendar calendar = Calendar.getInstance();
+                    int currentYear = calendar.get(Calendar.YEAR);
+
+                    int currentYearPosition = dates.indexOf(String.valueOf(currentYear));
+                    Log.d(TAG, "onTouch: Current year: " + currentYear + " Current year position: " + currentYearPosition);
+
+                    yearSpinner.setSelection(currentYearPosition);
+                }
+                return false;
+            }
+        });
+
+        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                yearSpinner.setSelection(0);
+            }
+        });
     }
 
     private void setReferences(){
         fruitsSpinner = findViewById(R.id.fruits_spinner);
         themesSpinner = findViewById(R.id.themes_spinner);
+        yearSpinner = findViewById(R.id.year_spinner);
+    }
+
+    private void createYearsListAdapter() {
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.get(Calendar.YEAR);
+        currentYear += 20;
+
+        dates.add(getString(R.string._year));
+
+        for (int i = 0; i < 100; i++){
+            int year = currentYear - i;
+            dates.add("" + year);
+        }
     }
 }
